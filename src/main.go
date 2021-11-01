f38eafd423d58b03eb40c8d2c916929c8d00b74b
cndoit18 <cndoit18@outlook.com>
2021-11-01 12:23:35 +0800

    add

---
+++ src/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	}))
+}
